364f2afb69870dbd850576a57bb62086e1c9af00
DhamoK <89825799+DhamoK@users.noreply.github.com>
2022-03-07 08:17:32 +0530

    Add files via upload

---
+++ Maven/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+    public String say(String message) {
+        return  "Hello World";
+    }
+}
