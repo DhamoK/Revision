a518f998359675446d2cdafdb63442276ed6fcac
DhamoK <89825799+DhamoK@users.noreply.github.com>
2022-03-07 08:10:10 +0530

    Add files via upload

---
+++ EmployeeSpringBoot/src/main/java/com/example/EmployeeSpringBoot/Employee.java
@@ -0,0 +1,38 @@
+package com.example.EmployeeSpringBoot;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ValueGenerationType;
+
+import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tbl_employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String department;
+
+
+}
